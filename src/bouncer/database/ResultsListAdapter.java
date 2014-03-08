09ea925d9d1ffad3888be8d4fd6cea3dba42caa3
tomekl007 <tomekl007@gmail.com>
2014-03-08 23:44:41 +0100

    change name of packege

--- src/bouncer/database/ResultsListAdapter.java
+++ src/bouncer/database/ResultsListAdapter.java
@@ -1,19 +1,14 @@
 package bouncer.database;
 
 
-
-
-
-
-import bouncer.logic.R;
 import android.content.Context;
 import android.database.Cursor;
-import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView.FindListener;
+import android.widget.CursorAdapter;
 import android.widget.TextView;
+import bouncer.logic.R;
 
 public class ResultsListAdapter extends CursorAdapter {
 	
